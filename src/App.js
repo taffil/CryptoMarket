bbcc9142eb33f40e1b8f6898c8963784e3c2092e
Tafil Osmani <tafil.osmani3@gmail.com>
2021-08-31 17:14:10 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My very first React App
+          My very firt React App
         </p>
         <a
           className="App-link"
