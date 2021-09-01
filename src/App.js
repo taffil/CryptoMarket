074b9868df0a85d0745ef8ab9aded9782e2694ba
Tafil Osmani <tafil.osmani3@gmail.com>
2021-09-01 23:25:24 +0200

    comming soon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My very first React App
+          Comming Soon...
         </p>
         <a
           className="App-link"
