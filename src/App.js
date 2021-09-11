ba11cbfa41601f3c8beb6598816540694358e665
Tafil Osmani <tafil.osmani3@gmail.com>
2021-09-11 17:32:10 +0200

    my commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>CryptoMaret</h1>
+        <h1>CryptoMarket</h1>
       </header>
     </div>
   );
