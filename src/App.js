8e852b9837563c397a692c9de73f976cd41cf0be
Tafil Osmani <tafil.osmani3@gmail.com>
2021-09-11 17:38:26 +0200

    null

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>CryptoMarket</h1>
-      </header>
-    </div>
-  );
+  return
 }
 
 export default App;
