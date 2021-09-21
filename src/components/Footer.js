e2dd32ed07d2ef39b095e0631e7fd743c3161c98
Tafil Osmani <tafil.osmani3@gmail.com>
2021-09-21 15:00:56 +0200

    done with footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,21 +5,20 @@ const Footer = () => {
   return (
     <div>
       <section className="footer">
-        <span className="github item">
+        <span className="github">
           Find an issue with this page?{" "}
-          <a href="https://github.com/taffil/Code" style={{ color: "#498afb" }}>
+          <a href="https://github.com/taffil/CryptoMarket" style={{ color: "#498afb" }}>
             Fix it on GitHub
           </a>
         </span>
         <hr />
-        <span className="copyright item"> Copyright © 2021 Crypto Market</span>
-        <br />
-        <span className="react item">
+        <span className="copyright"> Copyright © 2021 Crypto Market</span>
+        <span className="react">
           Created by{" "}
           <a href="https://github.com/taffil" style={{ color: "#498afb" }}>
             taffil{" "}
           </a>{" "}
-          with <img src="logo192.png" alt="" style={{ width: "20px" }} />{" "}
+          with {" "}<img src="logo192.png" alt="" style={{ width: "35px" }} />{" "}
         </span>
         <hr />
       </section>
